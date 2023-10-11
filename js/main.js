049ef3a2deaef62a0e8464b5c061303fdfb0cc85
bitzsalex <mblonglive@gmail.com>
2023-10-12 01:35:02 +0900

    Add Light/Dark theme switcher
    This commit will add the JavaScript functionality to toggle the
    light and dark theme. It also modifies some CSS rules for the
    theme toggler work as needed.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
 feather.replace()
+
+const themeToggler = document.querySelector("button.theme__toggler")
+
+const changeTheme = theme => {
+    if (theme === "dark") {
+        document.body.classList.add("dark-theme")
+        localStorage.setItem("theme", "dark")
+    } else {
+        document.body.classList.remove("dark-theme")
+        localStorage.setItem("theme", "light")
+    }
+}
+
+const toggleTheme = () => {
+    if (document.body.classList.contains("dark-theme")) {
+        changeTheme("light")
+        themeToggler.classList.remove("theme--dark")
+    } else {
+        changeTheme("dark")
+        themeToggler.classList.add("theme--dark")
+    }
+}
+
+const setTheme = () => {
+    let usersTheme = localStorage.getItem("theme")
+
+    if (!usersTheme) {
+        changeTheme(
+            window.matchMedia("(prefers-color-scheme: dark)").matches ?
+            "dark" : "light"
+        )
+    } else {
+        changeTheme(usersTheme)
+    }
+}
+
+setTheme()
+
+if (themeToggler)
+    themeToggler.addEventListener("click", toggleTheme)
