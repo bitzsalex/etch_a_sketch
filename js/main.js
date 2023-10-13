397757f1c0df957b615b2d6225b6d24a10f1d5d0
bitzsalex <mblonglive@gmail.com>
2023-10-13 19:29:30 +0900

    Add a function to generate grids on the canvas
    This commit will add a JS function that will first check if there
    is user set grid size value; if yes, it will generate that much
    grids; if not, it will generate 16 grids which is the default grid
    size.

--- js/main.js
+++ js/main.js
@@ -36,3 +36,42 @@ setTheme()
 
 if (themeToggler)
     themeToggler.addEventListener("click", toggleTheme)
+
+
+// GRID ITEMS GENERATING CODE
+// ==========================
+const DEFAULT_GRID_SIZE = 16
+
+// Getting all necessary elements from the DOM
+const canvas = document.querySelector("#canvas")
+const gridSizeInput = document.querySelector("#grid_size")
+const gridSizeLabel = document.querySelector("label[for='grid_size']")
+
+const createGridElement = width => {
+    let gridItem = document.createElement("div")
+    gridItem.style.width = width + "%"
+    return gridItem
+}
+
+const generateGrids = size => {
+    let maxWidth = 100 / size
+
+    for (itr = 1; itr <= size ** 2; itr++) {
+        gridItem = createGridElement(maxWidth)
+        canvas.appendChild(gridItem)
+    }
+}
+
+const setInitialGridSize = () => {
+    let gridSize = localStorage.getItem("gridSize") || DEFAULT_GRID_SIZE
+    
+    gridSizeInput.value = gridSize
+    gridSizeLabel.textContent = "X " + gridSize
+    generateGrids(gridSize)
+}
+
+// gridSizeInput.addEventListener("keyup", () => {
+//     console.log("the value has been updated")
+// })
+
+setInitialGridSize()
