e34679682f76460288453921df7ddf43b711545d
bitzsalex <mblonglive@gmail.com>
2023-10-14 18:39:06 +0900

    Fix canvas background on theme change
    This commit will fix the big that won't change the background of
    the canvas when the theme was changed.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,5 @@
 feather.replace()
 
-let currentTheme
-
-// getting themeToggler button
-const themeToggler = document.querySelector("button.theme__toggler")
-
-const changeTheme = theme => {
-    if (theme === "dark") document.body.classList.add("dark-theme")
-    else document.body.classList.remove("dark-theme")
-    currentTheme = theme
-}
-
-const toggleTheme = () => {
-    if (document.body.classList.contains("dark-theme")) {
-        changeTheme("light")
-        localStorage.setItem("theme", "light")
-    } else {
-        changeTheme("dark")
-        localStorage.setItem("theme", "dark")
-    }
-}
-
-const setTheme = () => {
-    let usersTheme = localStorage.getItem("theme")
-
-    if (!usersTheme) {
-        changeTheme(
-            window.matchMedia("(prefers-color-scheme: dark)").matches ?
-            "dark" : "light"
-        )
-    } else {
-        changeTheme(usersTheme)
-    }
-}
-
-// setting the initial theme
-setTheme()
-
-if (themeToggler)
-    themeToggler.addEventListener("click", toggleTheme)
-
 
 // GRID CODE
 // ==========================
@@ -414,3 +374,44 @@ setInitialGridSize()
 setInitialGridLines()
 setInitialColor("pen")
 setInitialColor("bg")
+
+
+
+// getting themeToggler button
+const themeToggler = document.querySelector("button.theme__toggler")
+
+const changeTheme = theme => {
+    if (theme === "dark") document.body.classList.add("dark-theme")
+    else document.body.classList.remove("dark-theme")
+}
+
+const toggleTheme = () => {
+    if (document.body.classList.contains("dark-theme")) {
+        changeTheme("light")
+        localStorage.setItem("theme", "light")
+    } else {
+        changeTheme("dark")
+        localStorage.setItem("theme", "dark")
+    }
+    // change only the bg color
+    setInitialColor("bg")
+}
+
+const setTheme = () => {
+    let usersTheme = localStorage.getItem("theme")
+
+    if (!usersTheme) {
+        changeTheme(
+            window.matchMedia("(prefers-color-scheme: dark)").matches ?
+            "dark" : "light"
+        )
+    } else {
+        changeTheme(usersTheme)
+    }
+}
+
+// setting the initial theme
+setTheme()
+
+if (themeToggler)
+    themeToggler.addEventListener("click", toggleTheme)
